43c1d2162e51b8b910ed410fd6dc0240aa8b9a67
michb0t <t.pham29@live.com.au>
2023-02-05 21:57:55 +1100

    app init

---
+++ app.js
@@ -0,0 +1,28 @@
+// TOGGLE SWITCH
+
+const toggleSwitch = document.querySelector(".theme-switch-wrapper")
+const body = document.querySelector("body")
+const themeIcon = document.querySelector("#icon")
+const headerImg = document.querySelector("#hero")
+
+// Use localStorage.getItem('theme') to check the current theme on page load. This way, if the user has previously selected a theme, it will be persisted upon refresh.
+
+const currentTheme = localStorage.getItem("theme") || "light"; 
+body.setAttribute("data-theme", currentTheme)
+
+
+function setTheme() {
+    darkMode() 
+}
+
+
+function darkMode() {
+    body.setAttribute('data-theme', (body.getAttribute('data-theme') === 'dark') ? 'light' : 'dark');
+    themeIcon.setAttribute("src", (themeIcon.getAttribute("src") === "images/icon-moon.svg") 
+    ? "images/icon-sun.svg"
+    : "images/icon-moon.svg"
+    );
+    localStorage.setItem('theme', body.getAttribute('data-theme'));
+}
+
+toggleSwitch.addEventListener("click",setTheme)
